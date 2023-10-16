13a3e44a62afd39bdf00c4e98fd3410a94ca3109
OtavioPortela <81724162+OtavioPortela@users.noreply.github.com>
2023-10-16 20:38:36 -0300

    teste atualização de deploy

--- app/routes.py
+++ app/routes.py
@@ -3,8 +3,8 @@ from flask import render_template
 @app.route('/')
 @app.route('/index')
 def index():
-    nome = 'Otavio Augusto'
-    dados = {'profissao': 'Analista de sistemas',
+    nome = 'Otavio Augusto Portela Luciano'
+    dados = {'profissao': 'Analista de sistemas jr',
              'idade': 26,
              'tempo': '4 anos'}
     return render_template('index.html', nome='Otavio Augusto', dados = dados)
